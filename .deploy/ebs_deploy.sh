1002b5143a271bf315e98db70b75dc43f6d10d60
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-09-03 16:34:45 +0530

    Update ebs_deploy.sh
    [skip ci]

--- .deploy/ebs_deploy.sh
+++ .deploy/ebs_deploy.sh
@@ -5,7 +5,7 @@ SERVICE=$1
 ENV=$2
 TAG_SUFFIX=$3
 TAG="$ENV.$TAG_SUFFIX"
-
+EBS_ENVIRONMENT=$4
 case $ENV in
   "DEV") S3_BUCKET="appirio-platform-dev";;
   "QA") S3_BUCKET="appirio-platform-qa";;
@@ -36,7 +36,8 @@ export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 # eb deploy
 cd .deploy
 eb init -r us-east-1 $SERVICE
-EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
+#EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
+EB_OUTPUT="$(eb deploy $EBS_ENVIRONMENT -l $TAG -r us-east-1)"
 echo $EB_OUTPUT
 if [[ $EB_OUTPUT =~ .*ERROR.* ]]
 then
