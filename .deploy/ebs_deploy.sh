96cde013d0ecd8f751cd1a9ac97eefb80ed83974
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-09-26 13:41:10 +0530

    Update ebs_deploy.sh
    [skip ci]

--- .deploy/ebs_deploy.sh
+++ .deploy/ebs_deploy.sh
@@ -39,7 +39,7 @@ cd .deploy
 eb init --region us-east-1 --platform docker-18.06.1-ce scorecard-tool
 #EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
 #EB_OUTPUT="$(eb deploy $EBS_ENVIRONMENT -l $TAG -r us-east-1)"
-EB_OUTPUT="$(eb deploy scorecard-tool-dev-pg -l $TAG -r us-east-1)"
+EB_OUTPUT="$(eb deploy scorecard-tool-prod-pg -l $TAG -r us-east-1)"
 
 
 echo $EB_OUTPUT
